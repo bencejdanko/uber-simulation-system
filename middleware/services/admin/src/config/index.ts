cdafe588d5e0c7ff4f5a85448b6399092b354f5d
andha8664 <anne.d.ha@sjsu.edu>
2025-05-07 13:06:11 -0700

    Delete middleware/services/admin/src/config/index.ts

--- middleware/services/admin/src/config/index.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const config = {
-  mongoUri: process.env.MONGO_URI,
-  jwtSecret: process.env.JWT_SECRET || 'supersecretkey',
-  port: process.env.PORT,
-};
