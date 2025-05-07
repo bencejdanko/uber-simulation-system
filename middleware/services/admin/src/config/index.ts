8b7d2d327963f70d621fa27d142c35f2ac6225b2
Bence Danko <bencejdanko@gmail.com>
2025-05-07 14:16:07 -0700

    remove ts config

--- middleware/services/admin/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const config = {
-  mongoUri: process.env.MONGO_URI,
-  jwtSecret: process.env.JWT_SECRET || 'supersecretkey',
-  redisUrl: process.env.REDIS_URL,
-  port: process.env.PORT,
-};
